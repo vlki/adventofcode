04a1d629b0964e449fbda295079ee946ca520942
Jan Vlcek <vlki@vlki.cz>
2015-12-27 11:46:07 +0100

    Solved 4

---
+++ 4/script1.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var md5 = require('md5-jkmyers');
+var secretKey = 'ckczppom';
+var i = 1;
+var hash;
+var testString;
+
+do {
+    console.log('checking num: ' + i);
+    testString = secretKey + i;
+    hash = md5(testString);
+    i++;
+} while(hash.substr(0, 5) !== '00000');
+
+console.log('md5 of "' + testString + '" is "' + hash + '"');
--- 4/script2.js
+++ 4/script2.js
@@ -7,6 +7,10 @@ var hash;
 var testString;
 
 do {
+    console.log('checking num: ' + i);
     testString = secretKey + i;
     hash = md5(testString);
-} while(hash.substr(0, 5) === '00000');
+    i++;
+} while(hash.substr(0, 6) !== '000000');
+
+console.log('md5 of "' + testString + '" is "' + hash + '"');
